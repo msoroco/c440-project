f55a4c858475a79448995a94a9a4a0dd1c9128ab
Frederick Shpilevskiy <f.shpilevskiy@gmail.com>
2023-03-29 23:30:01 -0700

    Fixed timing bug in main.py with the grid

--- main.py
+++ main.py
@@ -4,16 +4,8 @@ from matplotlib import animation
 from sim import Body, Spaceship
 
 BOX_WIDTH = 20
-GRID_RADIUS = 4
+GRID_RADIUS = 8
 DRAW_NEIGHBOURHOOD = True
-
-star = Body(30, np.array([0, 0], dtype=float), np.array([0, 0], dtype=float), 'orange')
-planet = Body(0, np.array([0, 150], dtype=float), np.array([1.25, 0], dtype=float), 'blue')
-spaceship = Spaceship(np.array([0, 160], dtype=float), np.array([1.25, 0], dtype=float), 'black')
-objective = np.array([-100, -100], dtype=float)
-
-bodies = [star, planet, spaceship]
-states = []
 
 # grid_radius = number of boxes from the center
 # TODO: add new channels for past frames
@@ -23,29 +15,36 @@ def get_state(agent, objective, bodies, grid_radius, box_width, frames=4, step_s
     objective_grid = np.zeros((2*grid_radius+1, 2*grid_radius+1))
     # assign objective
     # transform and shift to bottom left corner
-    position = (objective - agent.position) + radius*np.ones(2)
-    # position = np.sign(position)*np.minimum(radius*np.ones(2), np.abs(position))
-    # TODO: this all kind of depends how you want the grids to look (it's just a bunch of horizontal flips or whatever)
+    position = (objective - agent.position)
+    # TODO: rotation goes here
+    position = position + radius*np.ones(2)
     index = np.clip(np.floor(position/box_width).astype(int), 0, 2*grid_radius)
     objective_grid[index[0], index[1]] = 1
     # TODO: python-ize this (probably easy)
     for body in bodies:
         if body != agent:
             position = body.position - agent.position
-            # need to do transform for rotations
+            # TODO: rotation goes here
             if np.max(np.abs(position)) <= radius:
                 position = position + radius*np.ones(2)
                 index = np.clip(np.floor(position/box_width).astype(int), 0, 2*grid_radius)
                 obstacle_grid[index[0], index[1]] = 1
-    return np.stack((obstacle_grid, objective_grid), axis=0)           
-
+    return np.stack((obstacle_grid, objective_grid), axis=0)    
 ## example 
 # grid = get_state(spaceship, objective, bodies, GRID_RADIUS, BOX_WIDTH)
-# print(grid)
+       
 
-T = 1000
+star = Body(30, np.array([0, 0], dtype=float), np.array([0, 0], dtype=float), 'orange')
+planet = Body(0.00009, np.array([0, 150], dtype=float), np.array([1.25, 0], dtype=float), 'blue')
+spaceship = Spaceship(np.array([0, 160], dtype=float), np.array([1.25, 0], dtype=float), 'black')
+objective = np.array([-100, -100], dtype=float)
+
+bodies = [star, planet, spaceship]
+states = [get_state(spaceship, objective, bodies, GRID_RADIUS, BOX_WIDTH)]
+
+T = 1500
 for t in range(T):
-    spaceship.do_action(3)
+    # spaceship.do_action(3)
     # do steps
     for body in bodies:
         body.step()
@@ -63,9 +62,9 @@ def animate(end):
     start = max(0, end-100)
     ax.cla()
     for body in bodies:
-        ax.plot(body.history[start:end, 0], body.history[start:end, 1], ".", color=body.color)
+        ax.plot(body.history[start:end+1, 0], body.history[start:end+1, 1], ".", color=body.color)
         if isinstance(body, Spaceship) and DRAW_NEIGHBOURHOOD:
-            draw_state(states[end-1], body.history[end-1], ax)
+            draw_state(states[end], body.history[end], ax)
     ax.set_xlim(-300, 300)
     ax.set_ylim(-300, 300)
 
@@ -79,5 +78,5 @@ def draw_state(state, position, ax):
             ax.add_patch(plt.Rectangle((position[0] + i*BOX_WIDTH, position[1] + j*BOX_WIDTH), BOX_WIDTH, BOX_WIDTH, fill=False))
 
 
-anim = animation.FuncAnimation(fig, animate, frames = T + 1, interval = 1)
+anim = animation.FuncAnimation(fig, animate, frames = T+1, interval = 1)
 plt.show()

0e09aabc63a10726e3e025e557115b2cb5e1ea5c
msoroco <maurice.mati@outlook.ca>
2023-04-01 13:13:41 -0700

    demo simulator.py

---
+++ testingSim.py
@@ -0,0 +1,39 @@
+import numpy as np
+import random
+from simulator import Body, Spaceship, Simulator
+from animation import SimAnimation
+from replay import Transition, ReplayMemory
+from model import DQN
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+BOX_WIDTH = 20
+GRID_RADIUS = 8
+DRAW_NEIGHBOURHOOD = True
+
+T = 10
+
+
+## demonstration of the simulator interface
+if __name__ == '__main__':
+    sim = Simulator("./sim1.json")
+    
+    #### testing:
+    sim.start()
+    states = [sim.get_current_frame()]
+    
+    state, reward, terminated = sim.step(3)
+    states.append(sim.get_current_frame())
+    for i in range(T):
+        action = random.randint(0, 4)
+        next_state, reward, termianted = sim.step(action)
+        
+        if terminated == True:
+            break
+
+    anim = SimAnimation(sim.bodies, states, T)
+    ####
+    # state_shape, n_actions = sim.info()
